28c3a4b1cb783ed70011f5b8b94d2dfd4d1d3182
kokoh <kokohtan@gmail.com>
2017-09-07 20:09:51 +0700

    add vuefire-firebase-assigntofirebase done

--- client/src/components/Main.vue
+++ client/src/components/Main.vue
@@ -4,19 +4,54 @@
     <button type="button" @click="increment">+</button>
     <button type="button" @click="nambahDua(2)"> +2 </button>
 
+
+<!-- here to connect with firebase n vuefire -->
+    <h3>here new button </h3>
+      <button @click="addPlayerOne">Add 1</button>
+      <p>The button above has been clicked {{ playerone }} times.</p>
+<!-- end here -->
   </div>
 </template>
 
 <script>
+import firebase from 'firebase'
+import Vuefire from 'vuefire'
+import Vue from 'vue'
+
+Vue.use(Vuefire)
+
+var config = {
+  databaseURL: 'https://vuefire-69bb2.firebaseio.com',
+  projectId: 'vuefire-69bb2'
+}
+var firebaseApp = firebase.initializeApp(config)
+var db = firebaseApp.database()
+var dbRefBaby = db.ref('baby/1')
+
 import { mapMutations, mapActions } from 'vuex'
 
 export default {
+  data () {
+    return {
+      playerone: 0
+    }
+  },
+  firebase: {
+    firebaseDatas: dbRefBaby
+  },
   computed: {
     showState () {
       return this.$store.state.count
     }
   },
   methods: {
+    addPlayerOne () {
+      this.playerone += 1
+      // this.firebaseDatas = this.playerone
+      this.$firebaseRefs.firebaseDatas.set({
+        babyone: this.playerone
+      })
+    },
     ...mapMutations([
       'increment'
     ]),
